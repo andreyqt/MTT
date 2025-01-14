fd309d1206a6be11eb83bda05c08a67bb803dc7a
Andrei Kuzmin <Andreytko@mail.ru>
2025-01-14 11:56:28 +0300

    fix

--- src/main/java/tutorial/mtt/service/AbstractRequestSender.java
+++ src/main/java/tutorial/mtt/service/AbstractRequestSender.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+
 import org.springframework.web.client.RestTemplate;
 import tutorial.mtt.mapper.MonkeyTypeTestMapper;
 
